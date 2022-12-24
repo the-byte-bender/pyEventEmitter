bad97f23f996f5968a1043bad5108fe64d0c6f22
meatbag93 <cccefg2@gmail.com>
2022-12-24 10:27:42 +0300

    build: Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="event-emitter",
+    version="1.0.0",
+    description="A Python implementation of the Node.js EventEmitter class.",
+    author="Your Name",
+    author_email="your@email.com",
+    py_modules=["event_emitter"],
+)
